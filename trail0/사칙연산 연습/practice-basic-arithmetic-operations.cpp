d896e2bd74ebfed88d8b063c6b99e00858159fab
whereisrmsqhs <102295613+whereisrmsqhs@users.noreply.github.com>
2026-05-15 20:07:02 +0900

    [Passed] 사칙연산 연습 (8ms, 0MB)

---
+++ trail0/사칙연산 연습/practice-basic-arithmetic-operations.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 30;
+    int c = a + b;
+
+    cout << c / 8 << "\n";
+    cout << c % 8 << "\n";
+    return 0;
+}
