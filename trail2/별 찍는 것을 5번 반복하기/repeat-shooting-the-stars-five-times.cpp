072b7e398ceb09096b1a6b2a901e5c2c11dc896e
whereisrmsqhs <102295613+whereisrmsqhs@users.noreply.github.com>
2026-06-07 21:19:30 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (2ms, 0MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    
+    return 0;
+}
