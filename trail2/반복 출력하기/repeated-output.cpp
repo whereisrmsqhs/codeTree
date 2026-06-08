b515d518a92aac77165212a1e92e7b58a91a38a8
whereisrmsqhs <102295613+whereisrmsqhs@users.noreply.github.com>
2026-06-08 20:16:44 +0900

    [Passed] 반복 출력하기 (2ms, 0MB)

---
+++ trail2/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cout << "12345^&*()_\n";
+    }
+    return 0;
+}
