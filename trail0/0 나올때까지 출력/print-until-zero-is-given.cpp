6c0dd87b18a52ddc8269a9cd3c013a61acc9ff3a
whereisrmsqhs <102295613+whereisrmsqhs@users.noreply.github.com>
2026-08-08 22:18:10 +0900

    [Passed] 0 나올때까지 출력 (2ms, 0MB)

---
+++ trail0/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int num;
+    while (1) {
+        cin >> num;
+        if (num == 0) {
+            break;
+        }
+        cout << num << "\n";
+    }
+    return 0;
+}
